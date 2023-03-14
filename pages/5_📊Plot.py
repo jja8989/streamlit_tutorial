16501ec3609db44ed912a1f7763db8a970cd220f
jinwoo <jja8989@snu.ac.kr>
2023-03-14 09:16:21 +0900

    modified:   "pages/5_\360\237\223\212Plot.py"

--- pages/5_📊Plot.py
+++ pages/5_📊Plot.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("Plot")
 
@@ -15,3 +16,7 @@ with st.echo():
     st.area_chart(chart_data)
     st.bar_chart(chart_data)
 st.markdown("---")
+
+with st.echo():
+    fig = sns.boxplot(data=chart_data)
+st.pyplot(fig)
